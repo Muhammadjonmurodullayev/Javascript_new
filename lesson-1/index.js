a12e3d2ce5c59519d5707f0aa41164b0dc64392e
Muhammadjon.2005 <Muhammadjonmurodullayev2005@gmail.com>
2023-12-21 10:51:51 +0500

    Lesson-1

---
+++ lesson-1/index.js
@@ -0,0 +1,94 @@
+// let obj ={
+//     name :"ABdusamig'",
+//     surname:"academiy",
+
+// }
+// console.log(obj.name);
+// console.log(obj.key);
+// console.log(Object.keys(obj));
+// console.log(Object.values(obj));
+// console.log(Object.entries(obj));
+// }
+// console.log(obj.getname());
+// getname()
+// console.log(obj);
+// let {name,surname,chaild:{namee}}=obj
+// let  obj2 ={}
+// let obj2=obj
+// console.log(obj2);
+// let obj2=structuredClone(obj)
+// obj2.surname ="wba"
+// console.log(obj2);
+// let obj2 =structuredClone(obj)
+// console.log(namee);
+// for(let i=1;i<=100;i++){
+//     console.log(i);
+// }
+// let res = "Muhammadjon"
+// function getname(name){
+// console.log(`Men ismim ${name}`);
+// }
+// getname(5616)
+// Muhamadjon sozini o'zlashtirish uchun funksiya
+// function teskariQilish(str) {
+//   let teskari = '';
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     teskari += str[i];
+//   }
+
+//   return teskari;
+// }
+// let muhammadjon = 'Muhammadjon';
+// let teskariMuhammadjon = teskariQilish(muhammadjon)
+// console.log(teskariMuhammadjon);
+// let obj ={
+//     name:"Web",
+//     surname:"Muhamadjon",
+//     getname(){
+//         console.log(obj.name,obj.surname);
+//     }
+// }
+// obj.getname()
+// let {name,surname,chaild:{age}}=obj
+// let obj2=obj
+// let obj2=structuredClone(obj)
+// obj.name="bew"
+// console.log(age);
+// let  obj ={
+//     name:"web",
+//     surname:"academiy",
+// gatname(){
+//     console.log(this.name,this.surname);
+// }
+// }
+
+// obj.gatname()
+// function getname(){
+//     console.log(this);
+// }
+// getname()
+// let obj ={
+// name:"sait",
+// surname:"Muxtorov",
+// }
+// console.log(Object.keys(obj));
+// console.log(Object.values(obj));
+// console.log(Object.entries(obj));
+
+// let obj ={
+//     name:"wba",
+//     surname:"academiy"
+// }
+
+// // let obj2=obj
+// let obj2=structuredClone(obj)
+// obj.name="abw"
+// console.log(obj2);
+// function setname(name){
+//     console.log(`Mening ismi ${name}`);
+// }
+// setname("Abdulloh")
+// function setname(name1,name2,name3){
+//     console.log(name2,name1,name3);
+// }
+// setname("Abdulloh","saidkamol","asadbek")
