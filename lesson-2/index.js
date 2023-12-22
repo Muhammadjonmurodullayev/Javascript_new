27efffd0f0aba7cedb8bd4daf527a903b0bc1ce3
Muhammadjon.2005 <Muhammadjonmurodullayev2005@gmail.com>
2023-12-22 18:11:42 +0500

    Lesson-2

---
+++ lesson-2/index.js
@@ -0,0 +1,42 @@
+// let num =`124545.656562121`
+// let num2 =1_000_000_000
+
+// console.log(+num);
+// console.log(Number(num));
+// console.log(Number.parseInt(num));
+// console.log(parseInt(num));
+// console.log(parseFloat(num));
+// let a=12
+// a=13
+// console.log(a);
+// console.log(a);
+
+// let a=12
+// a++
+// console.log(a);
+// let b= 15+5
+// b++
+// b--
+// console.log(b);
+// b=14
+// console.log(a+b); 
+// console.log(a+b);
+// let str ="salom"
+// let res =""
+// for(i=str.length-1;i>=0;i--){
+//     res+=str[i]
+// }
+// console.log(res);
+// let a= 4.4464554
+// console.log(a.toFixed(5));
+// console.log(a.toPrecision(6));
+let obj = {
+    name:"Muhammadjon",
+    surname:"Murodullayev",
+    age:18,
+    chaild:{
+        name2:"Sardor",
+}}
+let {name,surname,age,chaild:{name2}}=obj 
+// console.log(Object.values(obj))
+// console.log(Object.keys(obj))
